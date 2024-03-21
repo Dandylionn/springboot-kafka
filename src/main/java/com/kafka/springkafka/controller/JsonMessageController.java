f54646345beba14cf72bc4f92e5b249f40288171
unknown <zheng.hui94@gmail.com>
2024-03-21 18:10:04 +0800

    updated config to fetch from properties file

--- src/main/java/com/kafka/springkafka/controller/JsonMessageController.java
+++ src/main/java/com/kafka/springkafka/controller/JsonMessageController.java
@@ -1,4 +1,26 @@
 package com.kafka.springkafka.controller;
 
+import com.kafka.springkafka.kafka.JsonKafkaProducer;
+import com.kafka.springkafka.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
 public class JsonMessageController {
+
+    private JsonKafkaProducer jsonKafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
 }
--- src/main/java/com/kafka/springkafka/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/kafka/springkafka/kafka/JsonKafkaConsumer.java
@@ -1,17 +1,18 @@
 package com.kafka.springkafka.kafka;
 
+import com.kafka.springkafka.payload.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaConsumer {
+public class JsonKafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
-    public  void consume(String message){
-        LOGGER.info(String.format("Message received -> %s", message));
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-idxxxx}")
+    public  void consume(User user){
+        LOGGER.info(String.format("Message received -> %s", user.toString()));
     }
 }
